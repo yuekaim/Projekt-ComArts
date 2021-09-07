21a12f1e686e721dc2382f9cd1a8d863dac7caec
yuekaim <yuekaim@gmail.com>
2021-09-07 17:24:46 +0200

    test

--- site/templates/activities.php
+++ site/templates/activities.php
@@ -3,9 +3,10 @@
 <h1><?= $page->title() ?></h1>
 <div class="content">
   <div class="post-list">
-    <?php $posts = $page->children() ?>
+    <?php $posts = $page->children()->published() ?>
     <?php foreach ($posts as $post): ?>
       <div class="activity">
+        test
         <h1><?= $post->headline() ?></h1>
         <span><?= $post->person() ?></span>
       </div>
