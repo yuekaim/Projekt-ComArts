b2b4cbb4dc931008276dc05a12c3d4222926e4eb
yuekaim <yuekaim@gmail.com>
2021-09-07 17:22:33 +0200

    test children

--- site/templates/activities.php
+++ site/templates/activities.php
@@ -3,7 +3,7 @@
 <h1><?= $page->title() ?></h1>
 <div class="content">
   <div class="post-list">
-    <?php $posts = $page->children()->published() ?>
+    <?php $posts = $page->children() ?>
     <?php foreach ($posts as $post): ?>
       <div class="activity">
         <h1><?= $post->headline() ?></h1>
