f78c178b5ed4a0982ea1bb6179d2c3ff6cd2ab84
yuekaim <yuekaim@gmail.com>
2021-09-07 17:17:12 +0200

    activities

--- site/templates/projekt.php
+++ site/templates/projekt.php
@@ -1,18 +1,17 @@
-<!doctype html>
 
-  <?php snippet('header') ?>
-  <body>
-    <h1><?= $page->title() ?></h1>
-    <div class="content">
-      <div class="post-list">
-        <?php $posts = $page->children()->listed() ?>
-        <?php foreach ($posts as $post):?>
-        <div class="activity">
-          <h1><?= $post->headline() ?></h1>
-          <span><?= $post->person() ?></span>
-        </div>
 
-      </div>
+<?php snippet('header') ?>
 
+<h1><?= $page->title() ?></h1>
+<div class="content">
+  <div class="post-list">
+    <?php $posts = $page->children()->listed() ?>
+    <?php foreach ($posts as $post):?>
+    <div class="activity">
+      <h1><?= $post->headline() ?></h1>
+      <span><?= $post->person() ?></span>
     </div>
-  </body>
+
+  </div>
+
+</div>
