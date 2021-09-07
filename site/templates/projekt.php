a065aa4da585ad0e93e382f7b8474ccccd86195e
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-09-07 23:15:51 +0800

    123

--- site/templates/projekt.php
+++ site/templates/projekt.php
@@ -5,7 +5,7 @@
     <h1><?= $page->title() ?></h1>
     <div class="content">
       <div class="post-list">
-        <?php $posts = $page->findPageOrDraft()->children()->listed() ?>
+        <?php $posts = $page->children()->listed() ?>
         <?php foreach ($posts as $post):?>
         <div class="activity">
           <h1><?= $post->headline() ?></h1>
