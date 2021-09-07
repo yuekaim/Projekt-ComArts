27084400294580c49e288cea243bdda7313a526e
yuekaim <yuekaim@gmail.com>
2021-09-07 16:44:01 +0200

    menu

--- site/templates/landing-page.php
+++ site/templates/landing-page.php
@@ -1,1 +1,5 @@
 <h1><?= $page->title() ?></h1>
+
+<a href="<?= $pages->find('projekt')->url() ?>">PROJEKTE</a>
+<a href="<?= $pages->find('')->url() ?>">PUBLIKATIONEN</a>
+<a href="<?= $pages->find('community')->url() ?>">ABOUT</a>
