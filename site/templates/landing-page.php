a029b3f44d1e98e23cfb47c19cbe5c0964545e3b
yuekaim <yuekaim@gmail.com>
2021-09-07 19:15:26 +0200

    text main

--- site/templates/landing-page.php
+++ site/templates/landing-page.php
@@ -3,3 +3,7 @@
 <a href="<?= $site->find('activities')->url() ?>">PROJEKTE</a>
 <a href="#">PUBLIKATIONEN</a>
 <a href="<?= $site->find('community')->url() ?>">ABOUT</a>
+
+<div class="info">
+  <p><?= $page->text() ?></p>
+</div>
