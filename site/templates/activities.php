76a17ec4b62d0881c330bca6a25f6ff737088202
yuekaim <yuekaim@gmail.com>
2021-09-07 19:30:19 +0200

    loop error

--- site/templates/activities.php
+++ site/templates/activities.php
@@ -22,20 +22,19 @@
           <?php $tags= $post->tags() ?>
           <span><?php echo $tags ?></span>
           <div class="content">
-          <?php foreach ($post->myBlocksField()->toBlocks() as $block): ?>
-            <div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
-              <?= $block ?>
-              </div>
-            <?php endforeach ?>
-          <div class="files">
-            <?php $pdfs =  $page->pdf()->toFiles();
-            foreach($pdfs as $pdf): ?>
-            <span id="download"><a href="<?= $pdf->url() ?>" download="proposed_file_name">Download PDF</a></span>
-            <?php endforeach ?>
+            <?php foreach ($post->myBlocksField()->toBlocks() as $block): ?>
+              <div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+                <?= $block ?>
+                </div>
+              <?php endforeach ?>
+            <div class="files">
+              <?php $pdfs =  $page->pdf()->toFiles();
+              foreach($pdfs as $pdf): ?>
+              <span id="download"><a href="<?= $pdf->url() ?>" download="proposed_file_name">Download PDF</a></span>
+              <?php endforeach ?>
 
-          </div>
             </div>
-          <?php endforeach ?>
+          </div>
         </div>
 
       </div>
