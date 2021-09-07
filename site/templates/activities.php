0fe089655de013d0a03e14a7708353ceed9f9189
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-09-08 00:46:07 +0800

    projekt

--- site/templates/activities.php
+++ site/templates/activities.php
@@ -6,8 +6,37 @@
     <?php $posts = $page->children()->published() ?>
     <?php foreach ($posts as $post): ?>
       <div class="activity">
-        <h1><?= $post->headline() ?></h1>
-        <span><?= $post->person() ?></span>
+        <div class="preview">
+          <?php if($image = $page->cover()->toFile()): ?>
+            <img src="<?= $image->url() ?>" alt="">
+          <?php endif ?>
+          <h1><?= $post->headline() ?></h1>
+          <span><?= $post->person() ?></span>
+          <span><?= $post->from() ?></span>
+          <span><?= $post->fromT() ?></span>
+          <span><?= $post->to() ?></span>
+          <span><?= $post->toT() ?></span>
+          <p><?= $post->metaInfo() ?></p>
+        </div>
+        <div class="main">
+          <?php $tags= $post->tags() ?>
+          <span><?= echo $tags?></span>
+          <div class="content">
+          <?php foreach ($post->myBlocksField()->toBlocks() as $block): ?>
+            <div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+              <?= $block ?>
+              </div>
+          <div class="files">
+            <?php $pdfs =  $page->pdf()->toFiles();
+            foreach($pdfs as $pdf): ?>
+            <span id="download"><a href="<?= $pdf->url() ?>" download="proposed_file_name">Download PDF</a></span>
+            <?php endforeach ?>
+
+          </div>
+            </div>
+          <?php endforeach ?>
+        </div>
+
       </div>
     <?php endforeach; ?>
 
