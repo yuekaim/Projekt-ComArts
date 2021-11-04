cfe879738f509a30cf66be36bdc045752e8783c4
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-05 02:33:28 +0800

    marquee

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -1,6 +1,9 @@
 <div class="body-flex">
 
 
-  <div class="top">
-    Kollaborative, kollektive, aktivistivi
+  <div class="marquee-top">
+    <div class="track">
+      <div class="content">&nbsp;Kollaborative, kollektive, aktivistivi</div>
+    </div>
   </div>
+</div>
