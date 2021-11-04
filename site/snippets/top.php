6f62be1b7a88a324cdcc67cd7007b4535ae829f6
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-05 03:14:50 +0800

    1

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -2,8 +2,8 @@
 <div class="body-flex">
 
 
-  <div class="top">
-    <div class="track">
+  <div id="panel" class="container top">
+    <div class="marquee">
       <div class="content">&nbsp;Kollaborative, kollektive, aktivistivi</div>
     </div>
   </div>
