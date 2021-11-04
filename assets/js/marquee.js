2ca5dff467ac444e02a4b9fb5ff4b6c88469cb71
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-05 03:09:51 +0800

    debug

--- assets/js/marquee.js
+++ assets/js/marquee.js
@@ -1,14 +1,14 @@
 $(function(){
 
   $('.top').hpanel({
-    duration: 100000;
-    padding: 10;
-    hover: false;
-    focus: true;
+    duration: 100000
+    padding: 10
+    hover: false
+    focus: true
     marquee_class: '.track',
     content_class: '.content',
-    fixed_class: 0;
-    
+    fixed_class: 0
+
   });
 
 });
