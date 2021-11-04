9f673a8f87eade8189298ebb822ff6de7fab91ba
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-05 03:27:38 +0800

    no plugin

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -3,19 +3,8 @@
 
 
   <div id="panel" class="container top">
-    <ul class="content">
-
-      <li><h1>Breaking News 1</h1></li>
-
-      <li><h1>Breaking News 2</h1></li>
-
-      <li><h1>Breaking News 3</h1></li>
-
-      <li><h1>Breaking News 4</h1></li>
-
-      <li><h1>Breaking News 5</h1></li>
-
-    </ul>
-
+    <div class="track">
+      <div class="content">&nbsp;Kollaborative, kollektive, aktivistivi</div>
+    </div>
   </div>
 </div>
