12c710421ad1c84ccbaa97ce79002b555eb47c4d
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-06 01:03:30 +0800

    if

--- assets/js/marquee.js
+++ assets/js/marquee.js
@@ -1,12 +1,15 @@
 function makeMarquee (){
-  const titleTop = document.querySelector('.top span').innerText|| element.textContent
-  const textTop = new Array(50).fill(titleTop).join('-')
-  const marqueeTop =document.querySelector('.top span')
-  marqueeTop.innerHTML = textTop
-
-  const titleBottom = document.querySelector('.bottom span').innerText|| element.textContent
-  const textBottom = new Array(50).fill(titleBottom).join('-')
-  const marqueeBottom =document.querySelector('.bottom span')
-  marqueeBottom.innerHTML = textBottom
+  if(document.querySelector('.top span').innerText != null){
+    const titleTop = document.querySelector('.top span').innerText|| element.textContent
+    const textTop = new Array(50).fill(titleTop).join('-')
+    const marqueeTop =document.querySelector('.top span')
+    marqueeTop.innerHTML = textTop
+}
+if(document.querySelector('.bottom span').innerText != null){
+    const titleBottom = document.querySelector('.bottom span').innerText|| element.textContent
+    const textBottom = new Array(50).fill(titleBottom).join('-')
+    const marqueeBottom =document.querySelector('.bottom span')
+    marqueeBottom.innerHTML = textBottom
+  }
 }
 makeMarquee()
