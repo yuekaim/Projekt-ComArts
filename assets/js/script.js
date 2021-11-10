bc07efc84b8896c803c386fda7b3c5913c07a43c
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-10 22:24:53 +0800

    menu position

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   $("*").click(function(){
     $("div.paperjs").hide()
     $("div.menu").css("display","flex")
+    $(".short-about").css("display","block")
   })
 
  })
