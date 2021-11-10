423eb7dc1e9957de037be012d3bdf5070dce1f66
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-11-10 23:12:04 +0800

    boxes

--- site/templates/landing-page.php
+++ site/templates/landing-page.php
@@ -16,7 +16,11 @@
     <button type="button">WEITER LESEN</button>
   </div>
 </div>
-<div class="headline"></div>
-<div class="news"></div>
+<div class="headline">
+  <span>placeholder</span>
+</div>
+<div class="news">
+  <span>placeholder</span>
+</div>
 
 <?php snippet('bottom') ?>
