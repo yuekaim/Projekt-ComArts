3414cd9b12fa3a50fc1ba0af45b90b010abd720a
yuekaim <yuekaim@gmail.com>
2021-11-10 18:08:09 +0100

    activites page

--- site/templates/activities.php
+++ site/templates/activities.php
@@ -2,30 +2,37 @@
 
 <?php snippet('header') ?>
 <?php snippet('top') ?>
-<?php snippet('menu') ?>
 
-<div class="content">
-  <div class="post-list">
-    <?php $posts = $page->children()->published() ?>
-    <?php foreach ($posts as $post): ?>
-      <div class="activity">
-        <div class="preview">
-          <?php if($image = $post->cover()): ?>
-            <img class="preview-img" src="<?= $image->url() ?>" alt="">
-          <?php endif ?>
-          <img class="preview-img" src="<?= $post->cover()->url() ?>" alt="">
-          <h1><a href=" "><?= $post->headline() ?></a></h1>
-          <span><?= $post->person() ?></span>
-          <span><?= $post->from() ?></span>
-          <span><?= $post->fromT() ?></span>
-          <span><?= $post->to() ?></span>
-          <span><?= $post->toT() ?></span>
-          <p><?= $post->metaInfo() ?></p>
-        </div>
+<div class="body-wrapper">
+  <?php snippet('menu') ?>
 
+  <div class="body-nomenu">
+
+    <div class="content">
+      <div class="post-list">
+        <?php $posts = $page->children()->published() ?>
+        <?php foreach ($posts as $post): ?>
+          <div class="activity">
+            <div class="preview">
+              <?php if($image = $post->cover()): ?>
+                <img class="preview-img" src="<?= $image->url() ?>" alt="">
+              <?php endif ?>
+              <img class="preview-img" src="<?= $post->cover()->url() ?>" alt="">
+              <h1><a href=" "><?= $post->headline() ?></a></h1>
+              <span><?= $post->person() ?></span>
+              <span><?= $post->from() ?></span>
+              <span><?= $post->fromT() ?></span>
+              <span><?= $post->to() ?></span>
+              <span><?= $post->toT() ?></span>
+              <p><?= $post->metaInfo() ?></p>
+            </div>
+
+
+          </div>
+        <?php endforeach; ?>
 
       </div>
-    <?php endforeach; ?>
+    </div>
 
   </div>
 </div>
