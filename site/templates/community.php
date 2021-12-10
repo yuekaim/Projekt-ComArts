e3e1dd6a7f5f387c5c4178cdf64ea04b4a95ab40
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-12-11 00:28:36 +0800

    test

--- site/templates/community.php
+++ site/templates/community.php
@@ -2,24 +2,32 @@
 <?php snippet('header') ?>
 <?php snippet('top') ?>
 
-
 <div class="body-wrapper">
   <?php snippet('menu') ?>
 
   <div class="body-nomenu">
-    <div class="community hflex">
-      <div class="left">
-        <p><?= $page->about() ?></p>
-      </div>
-      <div class="right">
-        <p><?= $page->teamKontakt() ?></p>
+
+    <div class="short-about">
+      <div class="content">
+        <h1><span>PROJRKT COMARTS</span></h1>
+        <p><span><?= $page->text() ?></span></p>
+        <button type="button">WEITER LESEN</button>
       </div>
     </div>
 
-
+    <div class="home-info hflex normal">
+        <div class="headline">
+          <h1><span>HEADLINE</span></h1>
+          <img class="close" src="assets/img/shrink.svg">
+        </div>
+        <div class="news">
+          <h1><span>NEWS</span></h1>
+          <img class="close" src="assets/img/shrink.svg">
+        </div>
     </div>
 
   </div>
 
+</div>
 
 <?php snippet('bottom') ?>
