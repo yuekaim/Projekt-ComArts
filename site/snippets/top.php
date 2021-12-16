48154e16669e79a70cbdedf142342106ace541f2
yuekaim <yuekaim@gmail.com>
2021-12-16 01:29:02 +0100

    Marquee Top

--- site/snippets/top.php
+++ site/snippets/top.php
@@ -5,6 +5,6 @@
 
   <div class="container">
     <div class="marquee top">
-      <span>test test test</span> <!-- marquee在其他页面显示不出来应该是这个kirby的路径不对，你看看-->
+      <span><?= $site->find('landing-page')->marqueeTop() ?></span> <!-- marquee在其他页面显示不出来应该是这个kirby的路径不对，你看看-->
     </div>
   </div>
