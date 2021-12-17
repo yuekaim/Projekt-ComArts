53701953e88ea92c0ac7eac3892d3b834681dafa
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-12-17 17:21:55 +0800

    kirby

--- site/templates/community.php
+++ site/templates/community.php
@@ -13,24 +13,27 @@
             <p><?= $page->about() ?></p>
 
         </div>
-        <div class="kontakt box">
+        <div class="kontakt">
 
-            <h1>Kontakt</h1>
+            <!-- <h1>Kontakt</h1> -->
+          <div class="projektleitung box">
             <?php if ($page->projektleitung()->isNotEmpty()): ?>
             <h3>Projektleitung</h3>
             <p><?= $page->projektleitung() ?></p>
             <?php endif ?>
-
+          </div>
+          <div class="wissenschaftlich box">
             <?php if ($page->wissenschaftlich()->isNotEmpty()): ?>
             <h3>Wissenschaftliche Bearbeitung und Koordination</h3>
             <p><?= $page->wissenschaftlich() ?></p>
             <?php endif ?>
-
+          </div>
+          <div class="fands box">
             <?php if ($page->fands()->isNotEmpty()): ?>
             <h3>Freie & Studierendenmitarbeiter:innen</h3>
             <p><?= $page->fands() ?></p>
             <?php endif ?>
-
+          </div>
 
 
             <!-- <p><?= $page->teamKontakt() ?></p> -->
