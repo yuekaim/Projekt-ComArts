f8cb078deb4443f7c150c1dfc9c1bc275f100120
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-12-27 01:15:25 +0800

    headline

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,6 +5,10 @@
   <?php snippet('menu') ?>
 
   <div class="body-nomenu">
+    <div class="headline">
+      <h1><span>HEADLINE</span></h1>
+      <img class="close" src="assets/img/shrink.svg">
+    </div>
 
     <div class="short-about">
       <div class="content">
@@ -15,9 +19,11 @@
     </div>
 
     <div class="home-info hflex normal">
-        <div class="headline">
-          <h1><span>HEADLINE</span></h1>
-          <img class="close" src="assets/img/shrink.svg">
+      <div class="short-about">
+        <div class="content">
+          <h1><span>PROJRKT COMARTS</span></h1>
+          <p><span><?= $page->text() ?></span></p>
+          <button type="button">WEITER LESEN</button>
         </div>
         <div class="news">
           <h1><span>NEWS</span></h1>
