faec77ea0858a8fdcac9dfc0660b6109fe330293
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-12-27 01:11:05 +0800

    h1

--- site/templates/community.php
+++ site/templates/community.php
@@ -19,25 +19,25 @@
             <!-- <h1>Kontakt</h1> -->
           <div class="projektleitung box">
             <?php if ($page->projektleitung()->isNotEmpty()): ?>
-            <h3>Projektleitung</h3>
+            <h1>Projektleitung</h3>
             <p><?= $page->projektleitung() ?></p>
             <?php endif ?>
           </div>
           <div class="wissenschaftlich box">
             <?php if ($page->wissenschaftlich()->isNotEmpty()): ?>
-            <h3>Wissenschaftliche Bearbeitung und Koordination</h3>
+            <h1>Wissenschaftliche Bearbeitung und Koordination</h3>
             <p><?= $page->wissenschaftlich() ?></p>
             <?php endif ?>
           </div>
           <div class="fands box">
             <?php if ($page->fands()->isNotEmpty()): ?>
-            <h3>Freie & Studierendenmitarbeiter:innen</h3>
+            <h1>Freie & Studierendenmitarbeiter:innen</h3>
             <p><?= $page->fands() ?></p>
             <?php endif ?>
           </div>
           <div class="netzwerk box">
             <?php if ($page->netzwerk()->isNotEmpty()): ?>
-            <h3>Netzwerk</h3>
+            <h1>Netzwerk</h3>
             <p><?= $page->netzwerk() ?></p>
             <?php endif ?>
           </div>
