3eb2eea962ae7be410871c1054e94f7a897cb5ae
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2021-12-27 01:29:52 +0800

    flex

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,19 +6,21 @@
 
   <div class="body-nomenu">
 
-    <div class="short-about">
-      <div class="content">
-        <h1><span>PROJRKT COMARTS</span></h1>
-        <p><span><?= $page->text() ?></span></p>
-        <button type="button">WEITER LESEN</button>
-      </div>
+    <div class="headline">
+      <h1><span>HEADLINE</span></h1>
+    </div>
+
     </div>
 
     <div class="home-info hflex normal">
-        <div class="headline">
-          <h1><span>HEADLINE</span></h1>
+      <div class="short-about">
+        <div class="content">
+          <h1><span>PROJRKT COMARTS</span></h1>
+          <p><span><?= $page->text() ?></span></p>
+          <button type="button">WEITER LESEN</button>
           <img class="close" src="assets/img/shrink.svg">
         </div>
+      </div>
         <div class="news">
           <h1><span>NEWS</span></h1>
           <img class="close" src="assets/img/shrink.svg">
