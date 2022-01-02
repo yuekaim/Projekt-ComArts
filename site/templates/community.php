8653c38dbb0a6df560061a52741c8891f990e97f
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-02 23:40:02 +0800

    wraper

--- site/templates/community.php
+++ site/templates/community.php
@@ -18,28 +18,36 @@
 
             <!-- <h1>Kontakt</h1> -->
           <div class="projektleitung box">
+            <div class="content">
             <?php if ($page->projektleitung()->isNotEmpty()): ?>
             <h1>Projektleitung</h3>
             <p><?= $page->projektleitung() ?></p>
             <?php endif ?>
           </div>
+          </div>
           <div class="wissenschaftlich box">
+            <div class="content">
             <?php if ($page->wissenschaftlich()->isNotEmpty()): ?>
             <h1>Wissenschaftliche Bearbeitung und Koordination</h3>
             <p><?= $page->wissenschaftlich() ?></p>
             <?php endif ?>
+            </div>
           </div>
           <div class="fands box">
+            <div class="content">
             <?php if ($page->fands()->isNotEmpty()): ?>
             <h1>Freie & Studierendenmitarbeiter:innen</h3>
             <p><?= $page->fands() ?></p>
             <?php endif ?>
+            </div>
           </div>
           <div class="netzwerk box">
+            <div class="content">
             <?php if ($page->netzwerk()->isNotEmpty()): ?>
             <h1>Netzwerk</h3>
             <p><?= $page->netzwerk() ?></p>
             <?php endif ?>
+            </div>
           </div>
 
             <!-- <p><?= $page->teamKontakt() ?></p> -->
