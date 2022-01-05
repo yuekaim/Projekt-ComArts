72fd74d591e019235f47c771cd5631a8bf7c0496
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-05 23:15:01 +0800

    2

--- site/templates/community.php
+++ site/templates/community.php
@@ -20,7 +20,7 @@
           <div class="projektleitung box">
             <div class="content">
             <?php if ($page->projektleitung()->isNotEmpty()): ?>
-            <h1>Projektleitung</h3>
+            <h1 lang="de">Projekt&shy;leitung</h3>
             <p><?= $page->projektleitung() ?></p>
             <?php endif ?>
           </div>
