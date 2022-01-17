bdd82174321e0c23b57feac5a8ba0b506b043c7a
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-17 23:32:56 +0800

    kirby text

--- site/templates/projekt.php
+++ site/templates/projekt.php
@@ -34,7 +34,7 @@
     border-radius: 2rem;font-size:0.7rem;border: 0px"><?= $tag ?></span>
           <?php endforeach ?>
         </div>
-        <span style="font-size: 0.7rem;"><?= $page->personen() ?></span>
+        <span style="font-size: 0.7rem;"><?= $page->personen()->kirbytext() ?></span>
         <?= $page->maincontent()->kirbytext() ?>
       </div>
     </div>
