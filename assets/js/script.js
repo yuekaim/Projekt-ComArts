3bac6ac26e87de664825c68e9048fce2f45cb415
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-18 04:40:51 +0800

    scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
       $(this).parent().css("height","10vh");
       $(this).attr('src', newSrc);
       $(this).parent().find("p, button").css("display","none");
-      //$(this).parent().find(".content").css("display":"inline-block","position":"absolute","left":"9%","top":"-20%")
+      $(this).parent().find(".content").css("display":"inline-block","position":"absolute","top":"0.5rem")
 
     }
     else {
