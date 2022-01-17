43504cbdf45571e049eafe1e415e855475ce2e12
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-17 23:14:16 +0800

    tags

--- site/templates/projekt.php
+++ site/templates/projekt.php
@@ -31,7 +31,7 @@
         <div class="keywords">
           <?php foreach ($page->tags()->split() as $tag): ?>
           <span style="background-color: #bbb;
-    border-radius: 2rem;"><?= $tag ?></span>
+    border-radius: 2rem;font-size:0.7rem"><?= $tag ?></span>
           <?php endforeach ?>
         </div>
         <?= $page->maincontent()->kirbytext() ?>
