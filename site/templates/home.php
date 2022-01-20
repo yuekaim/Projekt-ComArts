b7c78b15cdca70f5d14471ca18e2b8a648e6965b
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-21 01:22:33 +0800

    title

--- site/templates/home.php
+++ site/templates/home.php
@@ -66,6 +66,7 @@
         <div class="content">
           <h1><span>PROJEKT COMARTS</span></h1>
           <p><span><?= $page->text() ?></span></p>
+          <!-- url-->
           <button type="button"><a href="https://comarts.suborder.center/community">WEITER LESEN</a></button>
         </div>
       </div>
