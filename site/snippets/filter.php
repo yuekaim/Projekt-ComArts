27e04d59f549869d2021f709111bcd8df5fe09a5
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-21 01:38:11 +0800

    2

--- site/snippets/filter.php
+++ site/snippets/filter.php
@@ -5,16 +5,16 @@
   <button class="btn" onclick="filterSelection('podcast')">Podcast</button>
   <button class="btn" onclick="filterSelection('video')">Video</button> -->
   <a href="<?= $site->find('library')->url() ?>">
-    <div class="menu-item<?php if(Url::query(Url::current()) == '') echo ' highlighted"' ?>" id="all">Alles</div>
+    <div class="menu-item<?php if(Url::query(Url::current()) == '') echo ' highlighted"' ?>" id="all">alles</div>
   </a>
   <a href="<?= $site->find('library')->url() ?>?publikations">
-    <div class="menu-item<?php if(Url::query(Url::current()) == 'publikations=') echo ' highlighted"' ?>" id="podcasts">Lesen</div>
+    <div class="menu-item<?php if(Url::query(Url::current()) == 'publikations=') echo ' highlighted"' ?>" id="podcasts">lesen</div>
   </a>
   <a href="<?= $site->find('library')->url() ?>?podcasts">
-    <div class="menu-item<?php if(Url::query(Url::current()) == 'podcasts=') echo ' highlighted"' ?>" id="podcasts">Hören</div>
+    <div class="menu-item<?php if(Url::query(Url::current()) == 'podcasts=') echo ' highlighted"' ?>" id="podcasts">hören</div>
   </a>
   <a href="<?= $site->find('library')->url() ?>?videos">
-    <div class="menu-item<?php if(Url::query(Url::current()) == 'videos=') echo ' highlighted"' ?>" id="videos">Schauen</div>
+    <div class="menu-item<?php if(Url::query(Url::current()) == 'videos=') echo ' highlighted"' ?>" id="videos">schauen</div>
   </a>
 
 
