51c2ca8307a84d2f5c7d12489c7a5f6cd3f52dc3
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-21 19:09:12 +0800

    height

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
       $(this).parent().animate({height:'10vh'},"slow");
       $(this).attr('src', newSrc);
       $(this).parent().find("p, button").css("display","none");
-      $(this).parent().find("h1").css({"position":"absolute","top":"-0.5rem"},"slow");
+      $(this).parent().find("h1").css({"position":"absolute","top":"-0.5rem"};
 
     }
     else {
-      $(this).parent().css("height","40vh");
+      $(this).parent().animate({height:'40vh'},"slow");
       $(this).attr('src', oldSrc);
       $(this).parent().find("p, button").css("display","block");
-       $(this).parent().find("h1").css({"position":"initial","top":"1rem"},"slow");
+       $(this).parent().find("h1").css({"position":"initial","top":"1rem"});
     }
     $closed1 = !$closed1;
   })
