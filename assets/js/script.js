8aed830716e9e2c09409406fcad4338202a206c7
Xinyi0114 <39009243+Xinyi0114@users.noreply.github.com>
2022-01-21 19:11:00 +0800

    correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
       $(this).parent().animate({height:'10vh'},"slow");
       $(this).attr('src', newSrc);
       $(this).parent().find("p, button").css("display","none");
-      $(this).parent().find("h1").css({"position":"absolute","top":"-0.5rem"};
+      $(this).parent().find("h1").css({"position":"absolute","top":"-0.5rem"});
 
     }
     else {
